062e7ce864b510d0f7ac6d54663f052cdb401104
Nim <nimeshnott@hotmail.co.uk>
2025-12-03 20:44:09 +0800

    Initial Commit

---
+++ clocked-in/clocked_inApp.swift
@@ -0,0 +1,17 @@
+//
+//  clocked_inApp.swift
+//  clocked-in
+//
+//  Created by Nimesh on 03/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct clocked_inApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
